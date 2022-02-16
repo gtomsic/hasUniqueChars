8e1316ec3e8bfd35dccceafc4415c09b4ee5c959
Gabriel Tomsic <tomsicgabriel@gmail.com>
2022-02-16 10:18:10 -0700

    Problem solve

--- hasUniqueChars.js
+++ hasUniqueChars.js
@@ -1,1 +1,14 @@
 // Write your code below
+const checkTheValue = (input) => {
+  const splitVal = input.split('');
+  const storedSet = new Set(input);
+  const convertedVal = [...storedSet];
+  if (splitVal.length !== convertedVal) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+const hashUnique = checkTheValue('Gabriell');
+console.log(hashUnique);
